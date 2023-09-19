8a9e60c856af50f30d84789af6e0d9b55a0d6c8b
singularitti <singularitti@outlook.com>
2023-09-18 21:42:29 -0400

    Move the position of docs of `Cell`

--- src/cell.jl
+++ src/cell.jl
@@ -4,6 +4,11 @@ using StructEquality: @struct_hash_equal_isequal
 export Cell, natoms, atomtypes
 
 abstract type AbstractCell end
+@struct_hash_equal_isequal struct Cell{L,P,T} <: AbstractCell
+    lattice::Lattice{L}
+    positions::Vector{MVector{3,P}}
+    atoms::Vector{T}
+end
 """
     Cell(lattice, positions, atoms)
 
@@ -15,11 +20,6 @@ by a vector of ``N`` vectors with floating point values, where ``N`` is the numb
 Argument `atoms` is a list of ``N`` values, where the same kind of atoms
 need to be the same type.
 """
-@struct_hash_equal_isequal struct Cell{L,P,T} <: AbstractCell
-    lattice::Lattice{L}
-    positions::Vector{MVector{3,P}}
-    atoms::Vector{T}
-end
 function Cell(lattice, positions, atoms)
     if !(lattice isa Lattice)
         lattice = Lattice(lattice)
