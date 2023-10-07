82f08702b1246138661e72c27b159097a2ca493d
singularitti <singularitti@outlook.com>
2023-10-07 01:59:56 -0400

    Fix `show` for `Lattice`

--- src/show.jl
+++ src/show.jl
@@ -1,15 +1,18 @@
 function Base.show(io::IO, ::MIME"text/plain", lattice::Lattice)
     summary(io, lattice)
     println(io)
-    join(io, ' ' * join(row, "  ") * '\n' for row in eachrow(lattice.data))
+    join(
+        io,
+        ' ' * join(basisvector, "  ") * '\n' for basisvector in basisvectors(lattice.data)
+    )
     return nothing
 end
 function Base.show(io::IO, ::MIME"text/plain", cell::Cell)
     summary(io, cell)
     println(io)
     println(io, " lattice:")
-    for row in eachrow(cell.lattice)
-        println(io, "   ", join(row, "  "))
+    for basisvector in basisvectors(cell.lattice)
+        println(io, "   ", join(basisvector, "  "))
     end
     num_atom = natoms(cell)
     println(io, " $num_atom atomic positions:")
