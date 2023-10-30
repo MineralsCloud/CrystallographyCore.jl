874a4501d61738a5c3ac504428b6745183e074c2
singularitti <singularitti@outlook.com>
2023-10-30 16:26:26 -0400

    Update comments

--- src/lattice.jl
+++ src/lattice.jl
@@ -151,16 +151,20 @@ Base.firstindex(::Lattice) = 1
 
 Base.lastindex(::Lattice) = 9
 
+# You need this to let the broadcasting work.
 Base.:*(lattice::Lattice, x) = Lattice(parent(lattice) * x)
 Base.:*(x, lattice::Lattice) = lattice * x
 
+# You need this to let the broadcasting work.
 Base.:/(lattice::Lattice, x) = Lattice(parent(lattice) / x)
 
 Base.:+(lattice::Lattice) = lattice
+# You need this to let the broadcasting work.
 Base.:+(lattice::Lattice, x) = Lattice(parent(lattice) .+ x)
 Base.:+(x, lattice::Lattice) = lattice + x
 
 Base.:-(lattice::Lattice) = -one(eltype(lattice)) * lattice
+# You need this to let the broadcasting work.
 Base.:-(lattice::Lattice, x) = Lattice(parent(lattice) .- x)
 Base.:-(x, lattice::Lattice) = -lattice + x
 
