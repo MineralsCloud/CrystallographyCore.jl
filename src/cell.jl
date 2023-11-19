1461fd556ac37824490a6e497d79efc8ffa539c5
singularitti <singularitti@outlook.com>
2023-11-19 04:14:47 -0500

    Add `ReducedCoordinates` from v0.6.1 to src/cell.jl

--- src/cell.jl
+++ src/cell.jl
@@ -1,9 +1,23 @@
-using StaticArrays: MVector
+using StaticArrays: FieldVector, Size
 using StructEquality: @struct_hash_equal_isequal
 
-export Cell, natoms, atomtypes
+import StaticArrays: similar_type
+
+export ReducedCoordinates, CrystalCoordinates, Cell, natoms, atomtypes
+
+struct ReducedCoordinates{T} <: FieldVector{3,T}
+    x::T
+    y::T
+    z::T
+end
+const CrystalCoordinates = ReducedCoordinates
 
 abstract type AbstractCell end
+@struct_hash_equal_isequal mutable struct Cell{L,P,T} <: AbstractCell
+    lattice::Lattice{L}
+    positions::Vector{ReducedCoordinates{P}}
+    atoms::Vector{T}
+end
 """
     Cell(lattice, positions, atoms)
 
@@ -15,21 +29,15 @@ by a vector of ``N`` vectors with floating point values, where ``N`` is the numb
 Argument `atoms` is a list of ``N`` values, where the same kind of atoms
 need to be the same type.
 """
-@struct_hash_equal_isequal struct Cell{L,P,T} <: AbstractCell
-    lattice::Lattice{L}
-    positions::Vector{MVector{3,P}}
-    atoms::Vector{T}
-end
 function Cell(lattice, positions, atoms)
     if !(lattice isa Lattice)
         lattice = Lattice(lattice)
     end
-    if positions isa AbstractVector
-        P = eltype(Base.promote_typeof(positions...))
-        positions = collect(map(MVector{3,P}, positions))
-    else
-        throw(ArgumentError("`positions` must be a `Vector` of `Vector`s!"))
+    if length(positions) != length(atoms)
+        throw(DimensionMismatch("the lengths of atomic positions and atoms are different!"))
     end
+    P = reduce(promote_type, eltype.(positions))
+    positions = map(ReducedCoordinates{P} ∘ collect, positions)
     L, T = eltype(lattice), eltype(atoms)
     return Cell{L,P,T}(lattice, positions, atoms)
 end
@@ -44,3 +52,7 @@ atomtypes(cell::Cell) = unique(cell.atoms)
 Get the lattice of a `Cell`.
 """
 Lattice(cell::Cell) = cell.lattice
+
+# See https://juliaarrays.github.io/StaticArrays.jl/dev/pages/api/#StaticArraysCore.FieldVector
+similar_type(::Type{<:ReducedCoordinates}, ::Type{T}, s::Size{(3,)}) where {T} =
+    ReducedCoordinates{T}
