aacf9f3614970295b7b93e90c583b87c10ea0092
singularitti <singularitti@outlook.com>
2023-12-13 02:23:13 -0500

    Add "Test broadcasting for reciprocal lattices"

--- test/lattice.jl
+++ test/lattice.jl
@@ -95,7 +95,7 @@ using Unitful, UnitfulAtomic
     end
 end
 
-@testset "Test broadcasting" begin
+@testset "Test broadcasting for lattices" begin
     lattice = Lattice([1, 0, 0], [0, 1, 0], [0, 0, 1])
     @test lattice .* 4 == 4 .* lattice == Lattice([4, 0, 0], [0, 4, 0], [0, 0, 4])
     @test lattice .* 4.0 == 4.0 .* lattice == Lattice([4.0, 0, 0], [0, 4.0, 0], [0, 0, 4.0])
@@ -111,4 +111,14 @@ end
         Lattice(
             [1u"nm", 0u"nm", 0u"nm"], [0u"nm", 1u"nm", 0u"nm"], [0u"nm", 0u"nm", 1u"nm"]
         )
+    @test_throws ArgumentError 4 / lattice
+    @test_throws ArgumentError 4.0 ./ lattice
+end
+
+@testset "Test broadcasting for reciprocal lattices" begin
+    a, b, c = 4, 3, 5
+    lattice = Lattice([a, -b, 0] / 2, [a, b, 0] / 2, [0, 0, c])
+    @test reciprocal(lattice .* 4) == reciprocal(lattice) ./ 4
+    @test_throws ArgumentError 4 / reciprocal(lattice)
+    @test_throws ArgumentError 4.0 ./ reciprocal(lattice)
 end
