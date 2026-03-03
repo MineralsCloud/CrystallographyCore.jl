80a0eca64b2b6af03eea3ee5224979ed48db7be5
singularitti <singularitti@outlook.com>
2026-03-03 04:33:53 -0600

    Extend `SpglibCell` from `Cell`

--- ext/SpglibExt.jl
+++ ext/SpglibExt.jl
@@ -2,7 +2,7 @@ module SpglibExt
 
 using CrystallographyCore: Cell
 
-import Spglib: unwrap_convert
+import Spglib: SpglibCell, unwrap_convert
 
 function unwrap_convert(cell::Cell)
     lattice, positions, atoms = cell.lattice, cell.positions, cell.atoms
@@ -12,5 +12,7 @@ function unwrap_convert(cell::Cell)
     catoms = collect(Cint, findfirst(==(atom), atomtypes) for atom in atoms)  # Mapping between unique atom types and atom indices
     return clattice, cpositions, catoms
 end
+
+SpglibCell(cell::Cell) = SpglibCell(cell.lattice, cell.positions, cell.atoms)
 
 end
